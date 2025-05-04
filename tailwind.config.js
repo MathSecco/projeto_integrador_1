c2a810cf6bc1cf2aec12b2e8e7a9d50b09c0a9a9
MathSecco <matheus.s.bezerra@outlook.com>
2025-05-04 16:32:51 -0300

    feat: add Tailwind CSS configuration and styles, update templates for improved layout and design

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./templates/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        fundo: "#fefae0",
+        primario: "#2f855a",
+        secundario: "#dd6b20",
+        destaque: "#2c5282",
+        texto: "#1a202c",
+      },
+    },
+  },
+  plugins: [],
+};
